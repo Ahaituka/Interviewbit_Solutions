d2865936ff56711616d080ddf7afb6e20fa0d424
Shubham Jain <bitloks@gmail.com>
2017-06-17 22:37:43 +0530

    ExcelColumnTitle

--- Level 2 Math/ExcelColumnTitle.cpp
+++ Level 2 Math/ExcelColumnTitle.cpp
@@ -1,4 +1,5 @@
-/*  Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+/* 
+ Given a positive integer, return its corresponding column title as appear in an Excel sheet.
 
 For example:
 
